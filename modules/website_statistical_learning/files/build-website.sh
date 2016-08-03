b94fec2a71b728ed3b4a8fcf2d98ee340c7065e3
JamesMcGuigan <james.mcguigan@gmail.com>
2016-08-03 16:46:28 +0100

    Fix deployment for statistical-learning

--- modules/website_statistical_learning/files/build-website.sh
+++ modules/website_statistical_learning/files/build-website.sh
@@ -2,12 +2,14 @@
 export HOME=/root   # node-gyp requires that the user's home directory is specified in either of the environmental variables HOME or USERPROFILE
 
 cd /root/github/statistical-learning/
+
 bundle install &&
 npm cache clean &&
-npm install &&
+npm install --no-optional &&  # --no-optional: fixes Not compatible with your operating system or architecture: fsevents@1.0.14
 npm list 2>&1 | awk '/missing:/ { print $4 }' | sed 's/,//' | xargs npm install &&
 yes | bower install --allow-root &&
 npm run production &&
+
 mkdir -p /root/build/ &&
 rm    -f /root/build/statistical-learning.tgz &&
 cd       /root/github/ &&
