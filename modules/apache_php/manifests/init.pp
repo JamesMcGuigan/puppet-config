cf8f86d644c2f8b8f38755d1a2d903974b16d4b2
JamesMcGuigan <james.mcguigan.github@gmail.com>
2017-04-20 00:28:10 +0100

    apache2 + php5 via apt-get and nginx proxy - now works

--- modules/apache_php/manifests/init.pp
+++ modules/apache_php/manifests/init.pp
@@ -1,8 +1,34 @@
 class apache_php {
-  package { ['apache2', 'libapache2-mod-php', 'php', 'php-mcrypt', 'libapache2-mpm-itk']:
-    ensure => installed
-  }
+  # Apache 2 + PHP5
+  # https://askubuntu.com/questions/756879/cant-install-php5-on-ubuntu-16-04
+  #
+  # sudo add-apt-repository ppa:ondrej/php
+  # sudo apt-get update
+  # sudo apt-get install php7.0 php5.6 php5.6-mysql php-gettext php5.6-mbstring php-xdebug libapache2-mod-php5.6 libapache2-mod-php7.0
+  # from php5.6 to php7.0: sudo a2dismod php5.6 ; sudo a2enmod php7.0 ; sudo service apache2 restart
+  # from php7.0 to php5.6: sudo a2dismod php7.0 ; sudo a2enmod php5.6 ; sudo service apache2 restart
 
+  # apt::ppa { 'ppa:ondrej/php': } ->
+  exec { "add-apt-repository ppa:ondrej/php":
+    command => "/usr/bin/add-apt-repository ppa:ondrej/php --yes",
+  } ->
+  package {[
+    'apache2',
+    'libapache2-mod-php',
+    'php',
+    'php-mcrypt',
+    'libapache2-mpm-itk',
+    'php7.0',
+    'php5.6',
+    'php5.6-mysql',
+    'php-gettext',
+    'php5.6-mbstring',
+    'php-xdebug',
+    'libapache2-mod-php5.6',
+    'libapache2-mod-php7.0'
+  ]:
+    ensure => installed
+  } ->
   service { 'apache2':
     ensure => running,
     enable => true
@@ -20,6 +46,7 @@ class apache_php {
     mode   => '0644',
     notify => Service['apache2'],
   }
+
   file { '/etc/apache2/mods-available/php7.0.conf':
     source => 'puppet:///modules/apache_php/mods-available/php7.0.conf',
     owner  => 'root',
@@ -27,15 +54,22 @@ class apache_php {
     notify => Service['apache2'],
   }
 
-
-
   file { '/etc/apache2/sites-enabled/000-default.conf':
     ensure => absent,
     notify => Service['apache2'],
   }
 
+  exec { "${name}: a2dismod php7.0; a2enmod php5.6 vhost_alias":
+    command => "/usr/sbin/a2dismod php7.0; /usr/sbin/a2enmod php5.6",
+    notify => Service['apache2'],
+  }
+  exec { "${name}: a2enmod vhost_alias":
+    command => "/usr/sbin/a2enmod vhost_alias",
+    notify => Service['apache2'],
+  }
+
   file { '/etc/apache2/mods-enabled/php7.0.conf':
-    ensure => link,
+    ensure => absent,
     target => "/etc/apache2/mods-available/php7.0.conf",
     notify => Service['apache2'],
   }
@@ -45,10 +79,8 @@ class apache_php {
     notify => Service['apache2'],
   }
   file { '/etc/apache2/mods-enabled/mod_mpm_prefork.so':
-    ensure => link,
+    ensure => absent,
     target => "/etc/apache2/mods-available/mod_mpm_prefork.so",
     notify => Service['apache2'],
   }
-
-
 }
--- modules/website_deploy/manifests/fixgov.pp
+++ modules/website_deploy/manifests/fixgov.pp
@@ -1,7 +1,7 @@
 class website_deploy::fixgov {
   website_deploy {'fixgov.org':
     config  => "php",
-    domains => ["fixgov.org", "www.fixgov.org"],
+    domains => ["fixgov.org", "www.fixgov.org", "fixgov.com", "www.fixgov.com"],
     source  => "https://github.com/JamesMcGuigan/fixgov.org.git"
   }
 }
